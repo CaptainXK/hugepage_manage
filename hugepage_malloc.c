6708eee689cdf84d87ea9545b2da9f680e6758f6
CaptainXK <402958518@qq.com>
2017-08-29 21:17:25 +0800

    add some annotation in hugepage_malloc

--- hugepage_malloc.c
+++ hugepage_malloc.c
@@ -24,6 +24,13 @@ void malloc_elem_mkend(hugepage_malloc_elem *elem, hugepage_malloc_elem *prev)
 }
 
 //find suitable free list id
+//    idx      |     size   
+//free_list[0]---(0,    2^ 8]
+//free_list[1]---(2^ 8, 2^10]
+//free_list[2]---(2^10, 2^12]
+//free_list[3]---(2^12, 2^14]
+//free_list[4]---(2^14, 2^16]
+//...
 size_t find_free_list_idx(size_t size)
 {
 	#define MALLOC_MINSIZE_LOG2 8
@@ -32,12 +39,27 @@ size_t find_free_list_idx(size_t size)
 	size_t log2;
 	size_t index;
 	
+	//if size <= 2^8, return 0
 	if(size <= (1UL << MALLOC_MINSIZE_LOG2))
 		return 0;
-
-	//find the smallest 2 ** (log2) >= size
+	
+	//find the smallest 2 ^ (log2) >= size
+	//__builtin_clzl(long var) : In binary bits array of var ,number of 0 before the first "1" from left to right.
+	//"log2" is the largest number that is power of 2 and smaller than "size"
+	//for example, when size is 4096
+	//the largest number, that is smaller than 4097 and is power of 2, is 4096
+	//so the "log2" is log2(4096) = 12
+	//unsigned long var consist of 64 bits
+	//how to calculate?(size = 4096)
+	//4096          = 00000000 00000000 00000000 00000000 00000000 00000000 00010000 00000000
+	//4096-1 = 4095 = 00000000 00000000 00000000 00000000 00000000 00000000 00001111 11111111
+	//log2 = 64 - 52 = 12 = log2(4096)  
 	log2 = sizeof(size) * 8 - __builtin_clzl(size-1);
 
+	//why "(log2-min_log2)+(increment - 1)/increment"?
+	//for example:
+	//we want to "7/2 = 3.5 = 4", but system will get 3 instead of 4
+	//so we add 7 by "increment-1" to get result we want 	
 	index = (log2 - MALLOC_MINSIZE_LOG2 + MALLOC_LOG2_INCREMENT - 1) / MALLOC_LOG2_INCREMENT;
 
 	return index <= MAX_FREE_LIST_NB - 1 ? index: MAX_FREE_LIST_NB - 1; 	
